144d382024e6daddfa772b1dd18b3f82351cb5a1
Zafar Ahasan <103955367+zafarD@users.noreply.github.com>
2023-10-16 12:51:39 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1001, 'Zafar', 'Learn Java Development', CURRENT_DATE(),false);
+
+insert into TODO (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1002, 'Zafar', 'Learn Full Stack Development', CURRENT_DATE(),false);
+
+insert into TODO (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1003, 'Zahid', 'Learn How to Gain Knowledge', CURRENT_DATE(),false);
+
+insert into TODO (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1004, 'Zahid', 'Learn To Become Stronger', CURRENT_DATE(),false);
---
+++ target/classes/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<%@ include file="common/header.jsp" %>
+<%@ include file="common/navigation.jsp" %>	
+
+<div class="container">
+	<h1>Welcome ${name}</h1>
+	<a href="list-todos">Manage</a> your todos
+</div>
+
+<%@ include file="common/footer.jsp" %>
